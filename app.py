cc851b2c1168efc7124b932203be93b5c97bec47
Krishna Sengar <81618299+krishnasengar@users.noreply.github.com>
2023-06-24 10:34:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Route to display the stored JSON data
 @app.route('/yoga', methods=['GET'])
-def get_data():
+def get_data_yoga():
     with open('data.json', 'r') as file:
         data = json.load(file)
     return jsonify(data)
